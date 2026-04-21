1f8e07d711dcecd4c3332444971c249d3263a914
Aditya16080 <adityaparekh200@gmail.com>
2026-04-21 15:00:44 +0200

    affiche la raison de pourquoi la connexion ne marche pas
    Improve error handling by including exception message.

--- db_connect.php
+++ db_connect.php
@@ -14,7 +14,7 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
-    die("Erreur connexion BDD");
+    die("Erreur connexion : ". $e->getMessage());
 }
 
 function getDB() {
