e1c0079c50bdfa04736ee5cf6bb2afc475b3b317
Relma3 <malikatouedraogo@gmail.com>
2026-04-25 19:29:46 +0200

    Update redirect path for admin dashboard

--- traitement_connexion.php
+++ traitement_connexion.php
@@ -58,7 +58,7 @@ if (isset($_POST['email'], $_POST['password'])) {
             } elseif ($user['type_utilisateur'] === 'prestataire') {
                 header('Location: dashboardP.php');
             } else {
-                header('Location: frontend/admin/dashboard.php?token=' . urlencode($token));
+                header('Location: /frontend/admin/dashboard.php?token=' . urlencode($token));
             }
             exit();
 
