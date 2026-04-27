8bb96d07827d0f571d1c1de3fb24a67072bd7047
Mmina <mmina.rathi@gmail.com>
2026-04-27 15:46:23 +0200

    création fichier temporaire test_pdf

--- test_pdf.php
+++ test_pdf.php
@@ -1,8 +1,4 @@
 <?php
-require_once 'check_session.php';
-if (!$is_connected || $_SESSION['type'] !== 'admin') {
-    die('Accès refusé');
-}
 require_once 'db_connect.php';
 require_once 'PdfGenerator.php';
 
