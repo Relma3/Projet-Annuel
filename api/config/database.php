c47394a25df0f074e561ecee5c2c12c91b087eea
Mmina <mmina.rathi@gmail.com>
2026-05-01 10:20:18 +0200

    correction database.php

--- api/config/database.php
+++ api/config/database.php
@@ -1,8 +1,9 @@
 <?php
 require_once __DIR__ . '/../../db_connect.php';
 
-function getDB(): PDO {
-    global $pdo;
-    return $pdo;
+if (!function_exists('getDB')) {
+    function getDB(): PDO {
+        global $pdo;
+        return $pdo;
+    }
 }
-?>
