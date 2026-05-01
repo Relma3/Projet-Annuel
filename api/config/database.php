89f17fde867f89c00ee554f397c12448edae95c2
Mmina <mmina.rathi@gmail.com>
2026-05-01 11:42:22 +0200

    modif fichiers act_devis database.php tutoriel.js

--- api/config/database.php
+++ api/config/database.php
@@ -1,8 +1,10 @@
 <?php
 require_once __DIR__ . '/../../db_connect.php';
 
-function getDB(): PDO {
-    global $pdo;
-    return $pdo;
+if (!function_exists('getDB')) {
+    function getDB(): PDO {
+        global $pdo;
+        return $pdo;
+    }
 }
 ?>
