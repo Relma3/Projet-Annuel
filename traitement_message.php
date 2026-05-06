0cdbb344d10152de15ad98b68b0af55f9b014682
Mmina <mmina.rathi@gmail.com>
2026-05-06 19:46:47 +0200

    modif traitement message

--- traitement_message.php
+++ traitement_message.php
@@ -22,11 +22,11 @@ require_once 'db_connect.php';
 
 $db = getDB();
 
-$stmt = $db->prepare("INSERT INTO messages (id_expediteur, id_destinataire, contenu, lu) VALUES (?, 4, ?, 0)");
+$stmt = $db->prepare("INSERT INTO messages (id_expediteur, id_destinataire, contenu, lu) VALUES (?, 2, ?, 0)");
 $stmt->execute([$_SESSION['id'], $contenu]);
 
 require_once __DIR__ . '/includes/send_notification.php';
-envoyerNotification($pdo, 4, 'Nouveau message', 'Vous avez reçu un nouveau message sur Silver Happy.', 'info');
+envoyerNotification($pdo, 2, 'Nouveau message', 'Vous avez reçu un nouveau message sur Silver Happy.', 'info');
 
 header('Location: messagerie.php?envoye=1');
 exit;
