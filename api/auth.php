520e99f8291a65f5beb0ff4df392ac465146aeea
Mmina <mmina.rathi@gmail.com>
2026-05-09 17:25:03 +0200

    gestion du mp oublié

--- api/auth.php
+++ api/auth.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . "/config/database.php";
 require_once __DIR__ . "/middleware.php";
+require_once __DIR__ . '/../vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
 
 function login() {
     $pdo = getDB();
